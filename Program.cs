6ec8cece5be088952bff6e33dd56c53415ae8bfe
Chris Davies <89serenity@gmail.com>
2016-03-25 16:13:57 +0000

    Largely finished. ThinBin will now add itself to the registry and run on start up. It will check every 4hrs for a new wallpaper.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Web.Script.Serialization;
+using Microsoft.Win32;
+using System.Reflection;
+using System.Linq;
 
 namespace ThinBing
 {
@@ -12,19 +15,59 @@ namespace ThinBing
 
         static void Main(string[] args)
         {
-            // Grab Bing JSON
-            string input = GrabData();
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
 
-            // Parse that magical wallpaper JSON
-            Bing data = ParseJson(input);
+            if (args.Contains("-h") || args.Contains("-help"))
+                PrintHelp();
 
-            // Grab that tasty image
-            string fileName = Path.Combine(Path.GetTempPath(), "ThinBing" + Path.GetExtension(data.images[0].url));
-            GrabWallpaper(BaseUrl + data.images[0].url, fileName);
+            if (args.Contains("-d"))
+            {
+                DelRegKey(rk);
+                Console.WriteLine("Deleting reg key and exiting...");
+                Environment.Exit(0);
+            }
 
-            // Now set that bad boy
-            SetWallpaper(fileName);
+            if (!CheckStartup(rk))
+                    AddStartup(rk);
 
+            // Run this magic forever and ever
+            while (true)
+            {
+                try
+                {
+                    // Grab Bing JSON
+                    string input = GrabData();
+
+                    // Parse that magical wallpaper JSON
+                    Bing data = ParseJson(input);
+
+                    // Grab that tasty image
+                    string fileName = Path.Combine(Path.GetTempPath(), "ThinBing" + Path.GetExtension(data.images[0].url));
+                    GrabWallpaper(BaseUrl + data.images[0].url, fileName);
+
+                    // Now set that bad boy
+                    SetWallpaper(fileName);
+                }
+
+                // If it fails, we will just wait a min until we try again
+                catch
+                {
+                    System.Threading.Thread.Sleep(60000);
+                }
+
+                // Checks every 4hrs
+                System.Threading.Thread.Sleep(14400 * 1000);
+            }
+
+        }
+
+        // None of this currently is of any use until I sort the console attach out
+        static void PrintHelp()
+        {
+            Console.WriteLine("ThinBing.exe [-h, -d]");
+            Console.WriteLine("-h, -help : This help");
+            Console.WriteLine("-d, -del  : Deletes the start up reg key and exits");
+            System.Environment.Exit(0);
         }
 
         static string GrabData()
@@ -56,6 +99,38 @@ namespace ThinBing
         static void SetWallpaper(String path)
         {
             SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+        static bool CheckStartup(RegistryKey reg)
+        {
+            object thinBing = Registry.GetValue(reg.Name, "ThinBing", null);
+
+            if (thinBing == null)
+                return false;
+            else if (thinBing.ToString() != '"' + Path.GetFullPath(Assembly.GetExecutingAssembly().Location) + '"')
+                return false;
+            else
+                return true;
+        }
+
+        static void AddStartup(RegistryKey reg)
+        {
+            object thinBing = Registry.GetValue(reg.Name, "ThinBing", null);
+
+            if (thinBing == null)
+                reg.SetValue("ThinBing", '"' + Path.GetFullPath(Assembly.GetExecutingAssembly().Location) + '"');
+            else if (thinBing != null && thinBing.ToString() != '"' + Path.GetFullPath(Assembly.GetExecutingAssembly().Location) + '"')
+            {
+                DelRegKey(reg);
+                reg.SetValue("ThinBing", '"' + Path.GetFullPath(Assembly.GetExecutingAssembly().Location) + '"');
+            }
+        }
+
+        static void DelRegKey(RegistryKey reg)
+        {
+            Console.WriteLine("Deleting registry key");
+            if (CheckStartup(reg))
+                reg.DeleteValue("ThinBing");
         }
     }
 }
