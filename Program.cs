d61345564ffd73ead1120706eab7c20ad7af09e2
Chris Davies <89serenity@gmail.com>
2016-03-28 10:39:32 +0100

    Retabbed everything as I hadn't realised VS had been spacing it up. Moved all my dll loads to NativeMethods. Added other process detection so only one ThinBing will run at a time. Fixed console writing, so if you run it from a console, you will get some output.

--- Program.cs
+++ Program.cs
@@ -1,145 +1,178 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Web.Script.Serialization;
-using Microsoft.Win32;
-using System.Reflection;
-using System.Linq;
 
 namespace ThinBing
 {
-    class Program
-    {
-        static string BaseUrl = "http://www.bing.com/";
+	class Program
+	{
+		internal static class NativeMethods
+		{
+			// Allows us to print to the console
+			[DllImport("kernel32.dll")]
+			internal static extern bool AttachConsole(int dwProcessId);
+			internal const int ATTACH_PARENT_PROCESS = -1;
 
-        static void Main(string[] args)
-        {
-            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-            int fail = 0;
+			[DllImport("kernel32.dll")]
+			internal static extern bool FreeConsole();
 
-            if (args.Contains("-h") || args.Contains("-help"))
-                PrintHelp();
+			[DllImport("user32.dll", CharSet = CharSet.Auto)]
+			internal static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
 
-            if (args.Contains("-d"))
-            {
-                DelRegKey(rk);
-                Console.WriteLine("Deleting reg key and exiting...");
-                Environment.Exit(0);
-            }
+			internal static readonly UInt32 SPI_SETDESKWALLPAPER = 0x14;
+			internal static readonly UInt32 SPIF_UPDATEINIFILE = 0x01;
+			internal static readonly UInt32 SPIF_SENDWININICHANGE = 0x02;
+		}
 
-            if (!CheckStartup(rk))
-                    AddStartup(rk);
+		static string BaseUrl = "http://www.bing.com/";
+		static string path = '"' + Path.GetFullPath(Assembly.GetExecutingAssembly().Location) + '"';
 
-            // Run this magic forever and ever
-            while (true)
-            {
-                try
-                {
-                    if (fail > 2)
-                    {
-                        Console.WriteLine("Failed too many times. Exiting...");
-                        Environment.Exit(-1);
-                    }
+		static void Main(string[] args)
+		{
+			NativeMethods.AttachConsole(NativeMethods.ATTACH_PARENT_PROCESS);
+			Console.WriteLine();
 
-                    // Grab Bing JSON
-                    string input = GrabData();
+			RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+			int fail = 0;
 
-                    // Parse that magical wallpaper JSON
-                    Bing data = ParseJson(input);
+			if (args.Contains("-h") || args.Contains("-help"))
+				PrintHelp();
 
-                    // Grab that tasty image
-                    string fileName = Path.Combine(Path.GetTempPath(), "ThinBing" + Path.GetExtension(data.images[0].url));
-                    GrabWallpaper(BaseUrl + data.images[0].url, fileName);
+			CheckForOtherProcess();
 
-                    // Now set that bad boy
-                    SetWallpaper(fileName);
-                    fail = 0;
-                }
+			if (args.Contains("-d"))
+			{
+				DelRegKey(rk);
+				Console.WriteLine("Deleting reg key and exiting...");
+				Environment.Exit(0);
+			}
 
-                // If it fails, we will just wait a min until we try again
-                catch
-                {
-                    System.Threading.Thread.Sleep(60000);
-                    fail++;
-                }
+			if (!CheckStartup(rk))
+					AddStartup(rk);
 
-                // Checks every 4hrs
-                System.Threading.Thread.Sleep(14400 * 1000);
-            }
+			// Run this magic forever and ever
+			while (true)
+			{
+				try
+				{
+					if (fail > 2)
+					{
+						Console.WriteLine("Failed too many times. Exiting...");
+						Environment.Exit(-1);
+					}
 
-        }
+					// Grab Bing JSON
+					string input = GrabData();
 
-        // None of this currently is of any use until I sort the console attach out
-        static void PrintHelp()
-        {
-            Console.WriteLine("ThinBing.exe [-h, -d]");
-            Console.WriteLine("-h, -help : This help");
-            Console.WriteLine("-d, -del  : Deletes the start up reg key and exits");
-            System.Environment.Exit(0);
-        }
+					// Parse that magical wallpaper JSON
+					Bing data = ParseJson(input);
 
-        static string GrabData()
-        {
-            WebClient web = new WebClient();
-            return web.DownloadString("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US");
-        }
+					// Grab that tasty image
+					string fileName = Path.Combine(Path.GetTempPath(), "ThinBing" + Path.GetExtension(data.images[0].url));
+					GrabWallpaper(BaseUrl + data.images[0].url, fileName);
 
-        static Bing ParseJson(string url)
-        {
-            JavaScriptSerializer js = new JavaScriptSerializer();
-            return js.Deserialize<Bing>(url);
-        }
+					// Now set that bad boy
+					SetWallpaper(fileName);
+					fail = 0;
+				}
 
-        static void GrabWallpaper(string url, string fileName)
-        {
-            WebClient web = new WebClient();
-            Console.WriteLine("Saving image to: {0}", fileName);
-            web.DownloadFile(url, fileName);
-        }
+				// If it fails, we will just wait a min until we try again
+				catch
+				{
+					System.Threading.Thread.Sleep(60000);
+					fail++;
+				}
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
+				// Checks every 4hrs
+				System.Threading.Thread.Sleep(14400 * 1000);
+			}
+		}
 
-        private static readonly UInt32 SPI_SETDESKWALLPAPER = 0x14;
-        private static readonly UInt32 SPIF_UPDATEINIFILE = 0x01;
-        private static readonly UInt32 SPIF_SENDWININICHANGE = 0x02;
+		static void CheckForOtherProcess()
+		{
+			foreach (Process proc in Process.GetProcessesByName("ThinBing"))
+			{
+				if (proc.Id != Process.GetCurrentProcess().Id)
+				{
+					Console.WriteLine("Killing old process: {0}", proc.Id);
+					proc.Kill();
+				}
+			}
+		}
 
-        static void SetWallpaper(String path)
-        {
-            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
-        }
+		static void PrintHelp()
+		{
+			Console.WriteLine();
+			Console.WriteLine("ThinBing.exe [-h, -d]");
+			Console.WriteLine("-h, -help : This help");
+			Console.WriteLine("-d, -del  : Deletes the start up reg key and exits");
+			Environment.Exit(0);
+		}
 
-        static bool CheckStartup(RegistryKey reg)
-        {
-            object thinBing = Registry.GetValue(reg.Name, "ThinBing", null);
+		static string GrabData()
+		{
+			WebClient web = new WebClient();
+			return web.DownloadString("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US");
+		}
 
-            if (thinBing == null)
-                return false;
-            else if (thinBing.ToString() != '"' + Path.GetFullPath(Assembly.GetExecutingAssembly().Location) + '"')
-                return false;
-            else
-                return true;
-        }
+		static Bing ParseJson(string url)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			return js.Deserialize<Bing>(url);
+		}
 
-        static void AddStartup(RegistryKey reg)
-        {
-            object thinBing = Registry.GetValue(reg.Name, "ThinBing", null);
+		static void GrabWallpaper(string url, string fileName)
+		{
+			WebClient web = new WebClient();
+			Console.WriteLine("Saving image to: {0}", fileName);
+			web.DownloadFile(url, fileName);
+		}
 
-            if (thinBing == null)
-                reg.SetValue("ThinBing", '"' + Path.GetFullPath(Assembly.GetExecutingAssembly().Location) + '"');
-            else if (thinBing != null && thinBing.ToString() != '"' + Path.GetFullPath(Assembly.GetExecutingAssembly().Location) + '"')
-            {
-                DelRegKey(reg);
-                reg.SetValue("ThinBing", '"' + Path.GetFullPath(Assembly.GetExecutingAssembly().Location) + '"');
-            }
-        }
+		static void SetWallpaper(String path)
+		{
+			NativeMethods.SystemParametersInfo(NativeMethods.SPI_SETDESKWALLPAPER, 0, path, NativeMethods.SPIF_UPDATEINIFILE | NativeMethods.SPIF_SENDWININICHANGE);
+		}
 
-        static void DelRegKey(RegistryKey reg)
-        {
-            Console.WriteLine("Deleting registry key");
-            if (CheckStartup(reg))
-                reg.DeleteValue("ThinBing");
-        }
-    }
+		static bool CheckStartup(RegistryKey reg)
+		{
+			object thinBing = Registry.GetValue(reg.Name, "ThinBing", null);
+
+			if (thinBing == null)
+				return false;
+			else if (thinBing.ToString() != path)
+				return false;
+			else
+				return true;
+		}
+
+		static void AddStartup(RegistryKey reg)
+		{
+			object thinBing = Registry.GetValue(reg.Name, "ThinBing", null);
+			
+			if (thinBing == null)
+			{
+				Console.WriteLine("Adding start up registry key");
+				reg.SetValue("ThinBing", path);
+			}
+			else if (thinBing != null && thinBing.ToString() != path)
+			{
+				DelRegKey(reg);
+				Console.WriteLine("Chaing start up registry key");
+				reg.SetValue("ThinBing", path);
+			}
+		}
+
+		static void DelRegKey(RegistryKey reg)
+		{
+			Console.WriteLine("Deleting registry key");
+			if (CheckStartup(reg))
+				reg.DeleteValue("ThinBing");
+		}
+	}
 }
