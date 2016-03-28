b7b6b7dcecd329de3ee64a8da2d8033545988917
Chris Davies <89serenity@gmail.com>
2016-03-28 17:43:27 +0100

    How bing.cs fell out of source control I will never know

---
+++ Bing.cs
@@ -0,0 +1,44 @@
+﻿namespace ThinBing
+{
+    public class Bing
+    {
+        public Image[] images { get; set; }
+        public Tooltips tooltips { get; set; }
+    }
+
+    public class Tooltips
+    {
+        public string loading { get; set; }
+        public string previous { get; set; }
+        public string next { get; set; }
+        public string walle { get; set; }
+        public string walls { get; set; }
+    }
+
+    public class Image
+    {
+        public string startdate { get; set; }
+        public string fullstartdate { get; set; }
+        public string enddate { get; set; }
+        public string url { get; set; }
+        public string urlbase { get; set; }
+        public string copyright { get; set; }
+        public string copyrightlink { get; set; }
+        public bool wp { get; set; }
+        public string hsh { get; set; }
+        public int drk { get; set; }
+        public int top { get; set; }
+        public int bot { get; set; }
+        public H[] hs { get; set; }
+        public object[] msg { get; set; }
+    }
+
+    public class H
+    {
+        public string desc { get; set; }
+        public string link { get; set; }
+        public string query { get; set; }
+        public int locx { get; set; }
+        public int locy { get; set; }
+    }
+}
