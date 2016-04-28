5ef6f18cd773291e5d29328d2c21ebbc5cf95c15
Chris Davies <chrdav@microsoft.com>
2016-04-28 23:55:32 +0100

    Improved the logging function so it doesn't create log files bigger than 10MB

--- Resources/Log.cs
+++ Resources/Log.cs
@@ -6,13 +6,20 @@ namespace ThinBing.Resources
 	class Log
 	{
 		private string outputFilePath;
+        private int MaxLogSize = 10 * 1024 * 1024;
 
-		public Log(string path)
+
+        public Log(string path)
 		{
 			outputFilePath = path;
-			string[] welcomeMessage = new string[]{ "## ThinBing - Grey Man Software - Chris Davies 2016 ##", "#### thinbing.greymansoftware.com ####" };
-			File.WriteAllLines(outputFilePath, welcomeMessage);
+            CreateLogFile();
 		}
+
+        public void CreateLogFile()
+        {
+            string[] welcomeMessage = new string[] { "## ThinBing - Grey Man Software - Chris Davies 2016 ##", "## thinbing.greymansoftware.com ##" };
+            File.WriteAllLines(outputFilePath, welcomeMessage);
+        }
 
 		public void WriteLine(bool date, string message, params object[] sections)
 		{
@@ -22,18 +29,27 @@ namespace ThinBing.Resources
 
 		public void WriteLine(string message, params object[] sections)
 		{
-			using (StreamWriter file = new StreamWriter(outputFilePath, true))
-				file.WriteLine(String.Format(message, sections));
+            //using (StreamWriter file = new StreamWriter(outputFilePath, true))
+            //	file.WriteLine(String.Format(message, sections));
 
+            File.AppendAllText(outputFilePath, String.Format(message, sections) + "\r\n");
 			Console.WriteLine(message, sections);
 		}
 
 		public void WriteLine()
 		{
-			using (StreamWriter file = new StreamWriter(outputFilePath, true))
-				file.WriteLine();
-
-			Console.WriteLine();
+            File.AppendAllText(outputFilePath, "\r\n");
+            Console.WriteLine();
 		}
+
+        public void TruncateLog()
+        {
+            if (new FileInfo(outputFilePath).Length >= MaxLogSize)
+            {
+                File.Delete(outputFilePath);
+                CreateLogFile();
+                File.AppendAllText(outputFilePath, "# truncated log #");
+            }
+        }
 	}
 }
